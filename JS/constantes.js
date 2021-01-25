cfb1d49286ea8baf0e452e5234e2e6562276fd35
alnsuarez <alansuarezsan@gmail.com>
2021-01-24 20:04:05 -0600

    el dia de hoy vimos como exportar funciones con export, es necesario poner modules en el doc html

---
+++ JS/constantes.js
@@ -0,0 +1,10 @@
+
+
+export let usuario="Jon";
+let password ="qwerty"
+
+export const PI=Math.PI;
+
+export default function saludar(){
+    console.log("Hola Modulos ES6");
+}
---
+++ JS/modulos.js
@@ -0,0 +1,11 @@
+import saludar, {PI, usuario}from "./constantes.js";
+import{aritmetica} from "./aritmetica.js";
+console.log("Archivos modulos.js");
+
+console.log(PI);
+
+console.log(usuario);
+
+console.log(aritmetica.sumar(1,2));
+
+saludar();
